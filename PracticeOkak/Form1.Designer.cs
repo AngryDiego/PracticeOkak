18d185c7a1ac06632eba74a811f2b76619890cf8
DiegoVS <Diegoanchukov@gmail.com>
2025-08-31 14:12:26 +0200

    Femboy Update

--- PracticeOkak/Form1.Designer.cs
+++ PracticeOkak/Form1.Designer.cs
@@ -28,17 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.buttonEnter = new System.Windows.Forms.Button();
             this.labelEnter = new System.Windows.Forms.Label();
             this.textBoxLogin = new System.Windows.Forms.TextBox();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.labelERRLogin = new System.Windows.Forms.Label();
             this.labelERRPassword = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabelToReg = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonEnter
             // 
-            this.buttonEnter.Location = new System.Drawing.Point(320, 247);
+            this.buttonEnter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(55)))), ((int)(((byte)(144)))));
+            this.buttonEnter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(36)))), ((int)(((byte)(134)))));
+            this.buttonEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEnter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.buttonEnter.Location = new System.Drawing.Point(175, 240);
             this.buttonEnter.Name = "buttonEnter";
             this.buttonEnter.Size = new System.Drawing.Size(114, 73);
             this.buttonEnter.TabIndex = 0;
@@ -51,7 +59,8 @@
             // 
             this.labelEnter.AutoSize = true;
             this.labelEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelEnter.Location = new System.Drawing.Point(315, 72);
+            this.labelEnter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.labelEnter.Location = new System.Drawing.Point(170, 55);
             this.labelEnter.Name = "labelEnter";
             this.labelEnter.Size = new System.Drawing.Size(134, 29);
             this.labelEnter.TabIndex = 1;
@@ -59,7 +68,8 @@
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(274, 124);
+            this.textBoxLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxLogin.Location = new System.Drawing.Point(120, 109);
             this.textBoxLogin.Multiline = true;
             this.textBoxLogin.Name = "textBoxLogin";
             this.textBoxLogin.Size = new System.Drawing.Size(221, 35);
@@ -67,7 +77,8 @@
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(274, 181);
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxPassword.Location = new System.Drawing.Point(120, 166);
             this.textBoxPassword.Multiline = true;
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
@@ -77,8 +88,8 @@
             // labelERRLogin
             // 
             this.labelERRLogin.AutoSize = true;
-            this.labelERRLogin.ForeColor = System.Drawing.Color.Red;
-            this.labelERRLogin.Location = new System.Drawing.Point(520, 124);
+            this.labelERRLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelERRLogin.Location = new System.Drawing.Point(375, 112);
             this.labelERRLogin.Name = "labelERRLogin";
             this.labelERRLogin.Size = new System.Drawing.Size(44, 16);
             this.labelERRLogin.TabIndex = 4;
@@ -88,29 +99,55 @@
             // labelERRPassword
             // 
             this.labelERRPassword.AutoSize = true;
-            this.labelERRPassword.ForeColor = System.Drawing.Color.Red;
-            this.labelERRPassword.Location = new System.Drawing.Point(523, 181);
+            this.labelERRPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelERRPassword.Location = new System.Drawing.Point(375, 169);
             this.labelERRPassword.Name = "labelERRPassword";
             this.labelERRPassword.Size = new System.Drawing.Size(44, 16);
             this.labelERRPassword.TabIndex = 5;
             this.labelERRPassword.Text = "label2";
             this.labelERRPassword.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(5)))), ((int)(((byte)(118)))));
+            this.panel1.Controls.Add(this.linkLabelToReg);
+            this.panel1.Controls.Add(this.labelERRLogin);
+            this.panel1.Controls.Add(this.labelERRPassword);
+            this.panel1.Controls.Add(this.labelEnter);
+            this.panel1.Controls.Add(this.textBoxLogin);
+            this.panel1.Controls.Add(this.buttonEnter);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Location = new System.Drawing.Point(141, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 411);
+            this.panel1.TabIndex = 21;
+            // 
+            // linkLabelToReg
+            // 
+            this.linkLabelToReg.AutoSize = true;
+            this.linkLabelToReg.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(63)))), ((int)(((byte)(117)))));
+            this.linkLabelToReg.Location = new System.Drawing.Point(108, 332);
+            this.linkLabelToReg.Name = "linkLabelToReg";
+            this.linkLabelToReg.Size = new System.Drawing.Size(253, 16);
+            this.linkLabelToReg.TabIndex = 6;
+            this.linkLabelToReg.TabStop = true;
+            this.linkLabelToReg.Text = "Don\'t have account yet? Make a new one!";
+            this.linkLabelToReg.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(63)))), ((int)(((byte)(117)))));
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 475);
-            this.Controls.Add(this.labelERRPassword);
-            this.Controls.Add(this.labelERRLogin);
-            this.Controls.Add(this.textBoxPassword);
-            this.Controls.Add(this.textBoxLogin);
-            this.Controls.Add(this.labelEnter);
-            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -122,6 +159,8 @@
         private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Label labelERRLogin;
         private System.Windows.Forms.Label labelERRPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabelToReg;
     }
 }
 
--- PracticeOkak/RegistrationForm.Designer.cs
+++ PracticeOkak/RegistrationForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrationForm));
             this.labelLoginERR = new System.Windows.Forms.Label();
             this.labelEmailERR = new System.Windows.Forms.Label();
             this.textBoxLogin = new System.Windows.Forms.TextBox();
@@ -42,13 +43,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelLoginERR
             // 
             this.labelLoginERR.AutoSize = true;
-            this.labelLoginERR.ForeColor = System.Drawing.Color.Red;
-            this.labelLoginERR.Location = new System.Drawing.Point(520, 163);
+            this.labelLoginERR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelLoginERR.Location = new System.Drawing.Point(366, 148);
             this.labelLoginERR.Name = "labelLoginERR";
             this.labelLoginERR.Size = new System.Drawing.Size(44, 16);
             this.labelLoginERR.TabIndex = 11;
@@ -58,8 +63,8 @@
             // labelEmailERR
             // 
             this.labelEmailERR.AutoSize = true;
-            this.labelEmailERR.ForeColor = System.Drawing.Color.Red;
-            this.labelEmailERR.Location = new System.Drawing.Point(520, 108);
+            this.labelEmailERR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelEmailERR.Location = new System.Drawing.Point(366, 93);
             this.labelEmailERR.Name = "labelEmailERR";
             this.labelEmailERR.Size = new System.Drawing.Size(44, 16);
             this.labelEmailERR.TabIndex = 10;
@@ -68,7 +73,8 @@
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(271, 160);
+            this.textBoxLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxLogin.Location = new System.Drawing.Point(126, 145);
             this.textBoxLogin.Multiline = true;
             this.textBoxLogin.Name = "textBoxLogin";
             this.textBoxLogin.PasswordChar = '*';
@@ -77,7 +83,8 @@
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(271, 105);
+            this.textBoxEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxEmail.Location = new System.Drawing.Point(126, 90);
             this.textBoxEmail.Multiline = true;
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(221, 35);
@@ -87,7 +94,8 @@
             // 
             this.labelReg.AutoSize = true;
             this.labelReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelReg.Location = new System.Drawing.Point(312, 31);
+            this.labelReg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.labelReg.Location = new System.Drawing.Point(164, 24);
             this.labelReg.Name = "labelReg";
             this.labelReg.Size = new System.Drawing.Size(148, 29);
             this.labelReg.TabIndex = 7;
@@ -95,7 +103,11 @@
             // 
             // buttonEnter
             // 
-            this.buttonEnter.Location = new System.Drawing.Point(333, 329);
+            this.buttonEnter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(55)))), ((int)(((byte)(144)))));
+            this.buttonEnter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(36)))), ((int)(((byte)(134)))));
+            this.buttonEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEnter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.buttonEnter.Location = new System.Drawing.Point(179, 320);
             this.buttonEnter.Name = "buttonEnter";
             this.buttonEnter.Size = new System.Drawing.Size(114, 73);
             this.buttonEnter.TabIndex = 6;
@@ -106,8 +118,8 @@
             // labelConfpassERR
             // 
             this.labelConfpassERR.AutoSize = true;
-            this.labelConfpassERR.ForeColor = System.Drawing.Color.Red;
-            this.labelConfpassERR.Location = new System.Drawing.Point(520, 273);
+            this.labelConfpassERR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelConfpassERR.Location = new System.Drawing.Point(366, 258);
             this.labelConfpassERR.Name = "labelConfpassERR";
             this.labelConfpassERR.Size = new System.Drawing.Size(44, 16);
             this.labelConfpassERR.TabIndex = 15;
@@ -117,8 +129,8 @@
             // labelPasswordERR
             // 
             this.labelPasswordERR.AutoSize = true;
-            this.labelPasswordERR.ForeColor = System.Drawing.Color.Red;
-            this.labelPasswordERR.Location = new System.Drawing.Point(520, 218);
+            this.labelPasswordERR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.labelPasswordERR.Location = new System.Drawing.Point(366, 203);
             this.labelPasswordERR.Name = "labelPasswordERR";
             this.labelPasswordERR.Size = new System.Drawing.Size(44, 16);
             this.labelPasswordERR.TabIndex = 14;
@@ -127,7 +139,8 @@
             // 
             // textBoxPass2
             // 
-            this.textBoxPass2.Location = new System.Drawing.Point(271, 270);
+            this.textBoxPass2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxPass2.Location = new System.Drawing.Point(126, 255);
             this.textBoxPass2.Multiline = true;
             this.textBoxPass2.Name = "textBoxPass2";
             this.textBoxPass2.PasswordChar = '*';
@@ -136,7 +149,8 @@
             // 
             // textBoxPass1
             // 
-            this.textBoxPass1.Location = new System.Drawing.Point(271, 215);
+            this.textBoxPass1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(76)))), ((int)(((byte)(154)))));
+            this.textBoxPass1.Location = new System.Drawing.Point(126, 200);
             this.textBoxPass1.Multiline = true;
             this.textBoxPass1.Name = "textBoxPass1";
             this.textBoxPass1.Size = new System.Drawing.Size(221, 35);
@@ -145,71 +159,92 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(151, 273);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.label3.Location = new System.Drawing.Point(6, 258);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 16);
             this.label3.TabIndex = 19;
             this.label3.Text = "Confirm password";
-            this.label3.Visible = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(165, 218);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.label4.Location = new System.Drawing.Point(20, 203);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 16);
             this.label4.TabIndex = 18;
             this.label4.Text = "Enter password";
-            this.label4.Visible = false;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(185, 163);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.label5.Location = new System.Drawing.Point(40, 148);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(70, 16);
             this.label5.TabIndex = 17;
             this.label5.Text = "Enter login";
-            this.label5.Visible = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(177, 108);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(162)))), ((int)(((byte)(198)))));
+            this.label6.Location = new System.Drawing.Point(35, 96);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(85, 16);
             this.label6.TabIndex = 16;
             this.label6.Text = "Entern e-mail";
-            this.label6.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(5)))), ((int)(((byte)(118)))));
+            this.panel1.Controls.Add(this.labelReg);
+            this.panel1.Controls.Add(this.buttonEnter);
+            this.panel1.Controls.Add(this.textBoxPass2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBoxPass1);
+            this.panel1.Controls.Add(this.textBoxLogin);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBoxEmail);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.labelEmailERR);
+            this.panel1.Controls.Add(this.labelConfpassERR);
+            this.panel1.Controls.Add(this.labelLoginERR);
+            this.panel1.Controls.Add(this.labelPasswordERR);
+            this.panel1.Location = new System.Drawing.Point(145, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 411);
+            this.panel1.TabIndex = 20;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(637, 227);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
             // 
             // RegistrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.labelConfpassERR);
-            this.Controls.Add(this.labelPasswordERR);
-            this.Controls.Add(this.textBoxPass2);
-            this.Controls.Add(this.textBoxPass1);
-            this.Controls.Add(this.labelLoginERR);
-            this.Controls.Add(this.labelEmailERR);
-            this.Controls.Add(this.textBoxLogin);
-            this.Controls.Add(this.textBoxEmail);
-            this.Controls.Add(this.labelReg);
-            this.Controls.Add(this.buttonEnter);
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(774, 457);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RegistrationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RegistrationForm";
             this.Load += new System.EventHandler(this.RegistrationForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -229,5 +264,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
